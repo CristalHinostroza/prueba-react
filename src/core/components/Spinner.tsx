43969440b5579faf33fdd928d9a861b276240cf7
cristalhinostroza <cr.hinostroza@duocuc.cl>
2024-12-26 17:31:49 -0300

    se crearon las rutas absolutas

---
+++ src/core/components/Spinner.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Text, View } from 'react-native'
+import React from 'react'
+export const Spinner = () => {
+  return (
+    <View>
+      <Text>Loading...</Text>
+    </View>
+  )
+}
